6ddab343c058e0769d4d426816c9b3e5c906ea38
Greg Stickel <gregory.stickel@live.com>
2022-09-22 08:49:21 -0400

    Update 1

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,14 @@
 // Write your code below
+function isPalindrom(word) {
+    let j = word.length - 1
+    for (let i = 0; i < (word.length - 1) / 2; i++) {
+      console.log((word.length -1)/2);  
+      if (word[i] !== word[j]) {
+        return false
+      }
+      j--
+    }
+    return true
+  }
+
+  console.log(isPalindrom("noon"));
